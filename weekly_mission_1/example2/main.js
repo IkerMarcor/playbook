43db5d0b562c570a29fbf96a348b19a3b272c2bb
cuateverde <ikermarcor5@gmail.com>
2022-04-13 15:22:22 -0700

    Mi primer commit desde git :)

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,6 @@
+// node main.js
+
+const logger = require('./logger')
+
+logger.info('This is an informational message') //info puede ser ejecutado en console y a funciones
+logger.verbose('This is a verbose message') //verbose puede ser ejecutado solo con funciones
